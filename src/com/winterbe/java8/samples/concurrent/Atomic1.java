ecad1d6700b4940ce3f9e1024834e9ba6ccd6dec
EnadulShaheen <shaheen_cse08@yahoo.com>
2020-08-23 11:38:52 -0500

    New changes

--- src/com/winterbe/java8/samples/concurrent/Atomic1.java
+++ src/com/winterbe/java8/samples/concurrent/Atomic1.java
@@ -12,7 +12,7 @@ public class Atomic1 {
 //Testing fork
 // This is my second comment
 //3RD comment
-    //New update 1
+    //New update 3
     private static final int NUM_INCREMENTS = 1000;
 
     private static AtomicInteger atomicInt = new AtomicInteger(0);
